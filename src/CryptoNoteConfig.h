cb4ad6f4a322bd3826347592f6252387f5fa0853
Lithiumbit <soporte@lithiumbit.com>
2019-03-02 18:25:00 -0300

    Change In blocks count for blocks downloading to 200 blocks
    Change P2P max buffer write size to 64MB

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -49,7 +49,7 @@ const uint64_t GENESIS_BLOCK_REWARD                          = UINT64_C(11250000
 static_assert(EMISSION_SPEED_FACTOR <= 8 * sizeof(uint64_t), "Bad EMISSION_SPEED_FACTOR");
 
 const size_t   CRYPTONOTE_REWARD_BLOCKS_WINDOW               = 100;
-const size_t   CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE     = 100000; 
+const size_t   CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE     = 100000;
 const size_t   CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE_V2  = 20000;
 const size_t   CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE_V1  = 10000;
 const size_t   CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE_CURRENT = CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE;
@@ -59,15 +59,15 @@ const uint64_t MINIMUM_FEE                                   = UINT64_C(10000);
 const uint64_t DEFAULT_DUST_THRESHOLD                        = UINT64_C(10000);
 const uint64_t MAX_TRANSACTION_SIZE_LIMIT                          = 100000;
 
-const uint64_t DIFFICULTY_TARGET                             = 180; 
+const uint64_t DIFFICULTY_TARGET                             = 180;
 const uint64_t EXPECTED_NUMBER_OF_BLOCKS_PER_DAY             = 24 * 60 * 60 / DIFFICULTY_TARGET;
 const size_t   DIFFICULTY_WINDOW                             = 17;
 const size_t   DIFFICULTY_WINDOW_V1                          = 480;
 const size_t   DIFFICULTY_WINDOW_V2                          = 480;
-const size_t   DIFFICULTY_CUT                                = 0;  
+const size_t   DIFFICULTY_CUT                                = 0;
 const size_t   DIFFICULTY_CUT_V1                             = 60;
 const size_t   DIFFICULTY_CUT_V2                             = 60;
-const size_t   DIFFICULTY_LAG                                = 0; 
+const size_t   DIFFICULTY_LAG                                = 0;
 const size_t   DIFFICULTY_LAG_V1                             = 15;
 const size_t   DIFFICULTY_LAG_V2                             = 15;
 static_assert(2 * DIFFICULTY_CUT <= DIFFICULTY_WINDOW - 2, "Bad DIFFICULTY_WINDOW or DIFFICULTY_CUT");
@@ -79,9 +79,9 @@ const uint64_t MAX_BLOCK_SIZE_GROWTH_SPEED_DENOMINATOR       = 365 * 24 * 60 * 6
 const uint64_t CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_BLOCKS     = 1;
 const uint64_t CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_SECONDS    = DIFFICULTY_TARGET * CRYPTONOTE_LOCKED_TX_ALLOWED_DELTA_BLOCKS;
 
-const uint64_t CRYPTONOTE_MEMPOOL_TX_LIVETIME                = 60 * 60 * 24;     
-const uint64_t CRYPTONOTE_MEMPOOL_TX_FROM_ALT_BLOCK_LIVETIME = 60 * 60 * 24 * 7; 
-const uint64_t CRYPTONOTE_NUMBER_OF_PERIODS_TO_FORGET_TX_DELETED_FROM_POOL = 7;  
+const uint64_t CRYPTONOTE_MEMPOOL_TX_LIVETIME                = 60 * 60 * 24;
+const uint64_t CRYPTONOTE_MEMPOOL_TX_FROM_ALT_BLOCK_LIVETIME = 60 * 60 * 24 * 7;
+const uint64_t CRYPTONOTE_NUMBER_OF_PERIODS_TO_FORGET_TX_DELETED_FROM_POOL = 7;
 
 const size_t   FUSION_TX_MAX_SIZE                            = CRYPTONOTE_BLOCK_GRANTED_FULL_REWARD_ZONE_CURRENT * 30 / 100;
 const size_t   FUSION_TX_MIN_INPUT_COUNT                     = 12;
@@ -90,9 +90,9 @@ const size_t   FUSION_TX_MIN_IN_OUT_COUNT_RATIO              = 4;
 const uint32_t KEY_IMAGE_CHECKING_BLOCK_INDEX                = 0;
 const uint32_t UPGRADE_HEIGHT_V2                             = 1;
 const uint32_t UPGRADE_HEIGHT_V3                             = 30;
-const unsigned UPGRADE_VOTING_THRESHOLD                      = 90;               
-const uint32_t UPGRADE_VOTING_WINDOW                         = EXPECTED_NUMBER_OF_BLOCKS_PER_DAY;  
-const uint32_t UPGRADE_WINDOW                                = EXPECTED_NUMBER_OF_BLOCKS_PER_DAY;  
+const unsigned UPGRADE_VOTING_THRESHOLD                      = 90;
+const uint32_t UPGRADE_VOTING_WINDOW                         = EXPECTED_NUMBER_OF_BLOCKS_PER_DAY;
+const uint32_t UPGRADE_WINDOW                                = EXPECTED_NUMBER_OF_BLOCKS_PER_DAY;
 static_assert(0 < UPGRADE_VOTING_THRESHOLD && UPGRADE_VOTING_THRESHOLD <= 100, "Bad UPGRADE_VOTING_THRESHOLD");
 static_assert(UPGRADE_VOTING_WINDOW > 1, "Bad UPGRADE_VOTING_WINDOW");
 
@@ -114,8 +114,8 @@ const uint8_t  BLOCK_MAJOR_VERSION_3                         =  3;
 const uint8_t  BLOCK_MINOR_VERSION_0                         =  0;
 const uint8_t  BLOCK_MINOR_VERSION_1                         =  1;
 
-const size_t   BLOCKS_IDS_SYNCHRONIZING_DEFAULT_COUNT        =  10000;  
-const size_t   BLOCKS_SYNCHRONIZING_DEFAULT_COUNT            =  100;    
+const size_t   BLOCKS_IDS_SYNCHRONIZING_DEFAULT_COUNT        =  10000;
+const size_t   BLOCKS_SYNCHRONIZING_DEFAULT_COUNT            =  200;
 const size_t   COMMAND_RPC_GET_BLOCKS_FAST_MAX_COUNT         =  1000;
 
 const int      P2P_DEFAULT_PORT                              =  27001;
@@ -124,21 +124,21 @@ const int      RPC_DEFAULT_PORT                              =  38002;
 const size_t   P2P_LOCAL_WHITE_PEERLIST_LIMIT                =  1000;
 const size_t   P2P_LOCAL_GRAY_PEERLIST_LIMIT                 =  5000;
 
-const size_t   P2P_CONNECTION_MAX_WRITE_BUFFER_SIZE          = 32 * 1024 * 1024; 
+const size_t   P2P_CONNECTION_MAX_WRITE_BUFFER_SIZE          = 64 * 1024 * 1024; 
 const uint32_t P2P_DEFAULT_CONNECTIONS_COUNT                 = 8;
 const size_t   P2P_DEFAULT_WHITELIST_CONNECTIONS_PERCENT     = 70;
-const uint32_t P2P_DEFAULT_HANDSHAKE_INTERVAL                = 60;            
-const uint32_t P2P_DEFAULT_PACKET_MAX_SIZE                   = 50000000;     
+const uint32_t P2P_DEFAULT_HANDSHAKE_INTERVAL                = 60;
+const uint32_t P2P_DEFAULT_PACKET_MAX_SIZE                   = 50000000;
 const uint32_t P2P_DEFAULT_PEERS_IN_HANDSHAKE                = 250;
-const uint32_t P2P_DEFAULT_CONNECTION_TIMEOUT                = 5000;         
-const uint32_t P2P_DEFAULT_PING_CONNECTION_TIMEOUT           = 2000;          
-const uint64_t P2P_DEFAULT_INVOKE_TIMEOUT                    = 60 * 2 * 1000; 
-const size_t   P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT          = 5000;          
+const uint32_t P2P_DEFAULT_CONNECTION_TIMEOUT                = 5000;
+const uint32_t P2P_DEFAULT_PING_CONNECTION_TIMEOUT           = 2000;
+const uint64_t P2P_DEFAULT_INVOKE_TIMEOUT                    = 60 * 2 * 1000;
+const size_t   P2P_DEFAULT_HANDSHAKE_INVOKE_TIMEOUT          = 5000;
 
-const uint32_t  P2P_FAILED_ADDR_FORGET_SECONDS               = (60*60);    
-const uint32_t  P2P_IP_BLOCKTIME                             = (60*60*24);  
+const uint32_t  P2P_FAILED_ADDR_FORGET_SECONDS               = (60*60);
+const uint32_t  P2P_IP_BLOCKTIME                             = (60*60*24);
 const uint32_t  P2P_IP_FAILS_BEFORE_BLOCK                    = 10;
-const uint32_t  P2P_IDLE_CONNECTION_KILL_INTERVAL            = (5*60); 
+const uint32_t  P2P_IDLE_CONNECTION_KILL_INTERVAL            = (5*60);
 
 const char     P2P_STAT_TRUSTED_PUB_KEY[]                    = "";
 
@@ -150,7 +150,7 @@ struct CheckpointData {
   const char* blockId;
 };
 
-const std::initializer_list<CheckpointData> CHECKPOINTS = {  
+const std::initializer_list<CheckpointData> CHECKPOINTS = {
   {0,       "8b528c7f7111cc547f9d300c7244460d571454ad6eb7890f54461a0c6414949f" },
   {1,       "c7820db218127bc9242e7d5673c0048943a166317ae23b52df54a741bf34e564" },
   {10000,   "2ce2cba3f66440807a666a89e54b95cc005f18e550ed04bb330654d70139cb36" },
